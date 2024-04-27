5c3b34006867bf5ada79a6f871dc0a7f8b76b3ba
Darshkumar Jasani <jasanidarsh3@gmail.com>
2024-04-26 20:55:52 -0700

    File Added

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,34 @@
 import React from 'react'
 import './Header.css'
+import img from './profile2.jpeg'
 function Header() {
+
+
   return (
-    <div>Header</div>
+    <>
+      <div>
+        <div className='headerBody'>
+          <div>
+            <div class="patterns">
+              <svg width="100%" height="100%">
+                <defs> 
+                    <style>
+                @import url("https://fonts.googleapis.com/css?  family=Lora:400,400i,700,700i");
+                    </style>
+                </defs>        
+                <rect x="0" y="0" width="100%" height="100%" fill="url(#polka-dots)"> </rect>
+              <text x="48%" y="70%"  text-anchor="middle">
+                I'm Darshkumar Jasani
+              </text>
+            </svg>
+            </div>
+
+            
+          </div>
+          <img src = {img}/>
+        </div>
+      </div>
+    </>
   )
 }
 
